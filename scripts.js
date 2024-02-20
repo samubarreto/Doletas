f1ca1077cfe2fa4b341cfaef698998e35f56c5ac
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2024-02-20 18:36:27 -0300

    integrating to the api

--- scripts.js
+++ scripts.js
@@ -1,19 +1,25 @@
-const teste1 = document.getElementById('teste1')
-const teste2 = document.getElementById('teste2')
-const teste3 = document.getElementById('teste3')
-
-const fazerRequest = () => {
+const fazerRequestEPreencherjavascripts = () => {
   let request = fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
   request
     .then(response => response.json())
     .then(payload => {
-      teste1.innerHTML = payload.USDBRL.name
-      teste2.innerHTML = payload.EURBRL.name
-      teste3.innerHTML = payload.BTCBRL.name
+      document.getElementById('js-transaction-name-1-0').innerHTML = payload.USDBRL.code
+      document.getElementById('js-transaction-name-1-1').innerHTML = payload.USDBRL.codein
+      document.getElementById('js-stranger-coin-1-igual').innerHTML = parseFloat(payload.USDBRL.bid).toFixed(2);
+
+      document.getElementById('js-transaction-name-2-0').innerHTML = payload.EURBRL.code
+      document.getElementById('js-transaction-name-2-1').innerHTML = payload.EURBRL.codein
+      document.getElementById('js-stranger-coin-2-igual').innerHTML = parseFloat(payload.EURBRL.bid).toFixed(2);
+
+      document.getElementById('js-transaction-name-3-0').innerHTML = payload.BTCBRL.code
+      document.getElementById('js-transaction-name-3-1').innerHTML = payload.BTCBRL.codein
+      document.getElementById('js-stranger-coin-3-igual').innerHTML = parseFloat(payload.BTCBRL.bid).toFixed(2);
     });
 }
 
-setInterval(fazerRequest, 30000)
+fazerRequestEPreencherjavascripts();
+
+// setInterval(fazerRequestEPreencherjavascripts, 30000)
 
 // setInterval(() => {
 //   request
