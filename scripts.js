3ea08465786360714dd5848f34dbad826c7665bc
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2024-03-05 19:22:30 -0300

    cool front-changes

--- scripts.js
+++ scripts.js
@@ -3,19 +3,16 @@ const fazerRequestEPreencherjavascripts = () => {
   request
     .then(response => response.json())
     .then(payload => {
-      document.getElementById('js-transaction-name-1-0').innerHTML = payload.USDBRL.code
-      document.getElementById('js-transaction-name-1-1').innerHTML = payload.USDBRL.codein
-      document.getElementById('js-stranger-coin-1-igual').innerHTML = parseFloat(payload.USDBRL.bid).toFixed(2);
+      document.getElementById('js-transaction-name-1-0').innerHTML = `$1 ${payload.USDBRL.code}`;
+      document.getElementById('js-stranger-coin-1-igual').innerHTML = `R$ ${parseFloat(payload.USDBRL.bid).toFixed(2)}`;
 
-      document.getElementById('js-transaction-name-2-0').innerHTML = payload.EURBRL.code
-      document.getElementById('js-transaction-name-2-1').innerHTML = payload.EURBRL.codein
-      document.getElementById('js-stranger-coin-2-igual').innerHTML = parseFloat(payload.EURBRL.bid).toFixed(2);
+      document.getElementById('js-transaction-name-2-0').innerHTML = `€1 ${payload.EURBRL.code}`;
+      document.getElementById('js-stranger-coin-2-igual').innerHTML = `R$ ${parseFloat(payload.EURBRL.bid).toFixed(2)}`;
 
-      document.getElementById('js-transaction-name-3-0').innerHTML = payload.BTCBRL.code
-      document.getElementById('js-transaction-name-3-1').innerHTML = payload.BTCBRL.codein
-      document.getElementById('js-stranger-coin-3-igual').innerHTML = parseFloat(payload.BTCBRL.bid).toFixed(2);
+      document.getElementById('js-transaction-name-3-0').innerHTML = `₿1 ${payload.BTCBRL.code}`;
+      document.getElementById('js-stranger-coin-3-igual').innerHTML = `R$ ${parseFloat(payload.BTCBRL.bid).toFixed(2)}`;
     });
-}
+};
 
 fazerRequestEPreencherjavascripts();
 
