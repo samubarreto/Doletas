6ab194ba7a57005238d7d522e516532a85906272
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2024-03-09 10:40:34 -0300

    removed btc

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 const fazerRequestEPreencherjavascripts = () => {
-  let request = fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
+  let request = fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL')
   request
     .then(response => response.json())
     .then(payload => {
       document.getElementById('js-stranger-coin-1-igual').innerHTML = `R$ ${parseFloat(payload.USDBRL.bid).toFixed(2)}`;
       document.getElementById('js-stranger-coin-2-igual').innerHTML = `R$ ${parseFloat(payload.EURBRL.bid).toFixed(2)}`;
-      document.getElementById('js-stranger-coin-3-igual').innerHTML = `R$ ${parseFloat(payload.BTCBRL.bid).toFixed(2)}`;
+      document.getElementById('js-stranger-coin-3-igual').innerHTML = `R$ ${parseFloat(payload.GBPBRL.bid).toFixed(2)}`;
     });
 };
 
