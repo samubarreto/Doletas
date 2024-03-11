5526433464c2567db2d2b50cef7b4908569c5188
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2024-03-11 01:05:33 -0300

    fully dark and testing new design..

--- scripts.js
+++ scripts.js
@@ -1,15 +1,19 @@
-const fazerRequestEPreencherjavascripts = () => {
+function fillJs(payload) {
+  document.getElementById('js-stranger-coin-1-igual').innerHTML = `R$${parseFloat(payload.USDBRL.bid).toFixed(2)}`;
+  document.getElementById('js-stranger-coin-2-igual').innerHTML = `R$${parseFloat(payload.EURBRL.bid).toFixed(2)}`;
+  document.getElementById('js-stranger-coin-3-igual').innerHTML = `R$${parseFloat(payload.GBPBRL.bid).toFixed(2)}`;
+};
+
+const getCotacoes = () => {
   let request = fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL')
   request
     .then(response => response.json())
     .then(payload => {
-      document.getElementById('js-stranger-coin-1-igual').innerHTML = `R$ ${parseFloat(payload.USDBRL.bid).toFixed(2)}`;
-      document.getElementById('js-stranger-coin-2-igual').innerHTML = `R$ ${parseFloat(payload.EURBRL.bid).toFixed(2)}`;
-      document.getElementById('js-stranger-coin-3-igual').innerHTML = `R$ ${parseFloat(payload.GBPBRL.bid).toFixed(2)}`;
+      fillJs(payload);
     });
 };
 
-fazerRequestEPreencherjavascripts();
+getCotacoes();
 
 // setInterval(fazerRequestEPreencherjavascripts, 30000)
 
