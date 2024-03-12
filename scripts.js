0aa59f1bf424d13cd95523c71089856952486c7a
Samuel Pereira <samu.barreto2004@gmail.com>
2024-03-12 16:22:37 -0300

    better coding and names in general

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 function fillJs(payload) {
-  document.getElementById('js-stranger-coin-1-igual').innerHTML = `R$${parseFloat(payload.USDBRL.bid).toFixed(2)}`;
+  document.getElementById('js-res-1').innerHTML = `R$${parseFloat(payload.USDBRL.bid).toFixed(2)}`;
   document.getElementById('js-from-1').innerHTML = `${payload.USDBRL.code}`;
   document.getElementById('js-to-1').innerHTML = `${payload.USDBRL.codein}`;
 
-  document.getElementById('js-stranger-coin-2-igual').innerHTML = `R$${parseFloat(payload.EURBRL.bid).toFixed(2)}`;
+  document.getElementById('js-res-2').innerHTML = `R$${parseFloat(payload.EURBRL.bid).toFixed(2)}`;
   document.getElementById('js-from-2').innerHTML = `${payload.EURBRL.code}`;
   document.getElementById('js-to-2').innerHTML = `${payload.EURBRL.codein}`;
 
-  document.getElementById('js-stranger-coin-3-igual').innerHTML = `R$${parseFloat(payload.GBPBRL.bid).toFixed(2)}`;
+  document.getElementById('js-res-3').innerHTML = `R$${parseFloat(payload.GBPBRL.bid).toFixed(2)}`;
   document.getElementById('js-from-3').innerHTML = `${payload.GBPBRL.code}`;
   document.getElementById('js-to-3').innerHTML = `${payload.GBPBRL.codein}`;
 
