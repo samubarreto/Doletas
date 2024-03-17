e9e8bff8a8e8e83cd3a9eeac60f7f70d5759e88d
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2024-03-17 15:39:33 -0300

    formatarMoeda function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
-function freeConvert(num, value, convert) {
+function formatarMoeda(value) {
+  return value.toFixed(2).replace(/\./g, ',').replace(/\d(?=(\d{3})+,)/g, '$&.');
+}
 
-  document.getElementById(`js-ft-res-${num}`).innerHTML = `R$${(value * convert).toFixed(2)}`;
+function freeConvert(num, value, convert) {
+  document.getElementById(`js-ft-res-${num}`).innerHTML = `R$${formatarMoeda(value * convert)}`;
 
   if (value.toString().length > 7) {
     document.getElementById(`js-ft-res-${num}`).style.fontSize = ".7rem";
@@ -12,7 +15,7 @@ function freeConvert(num, value, convert) {
 function baseFill(num, convert, payload) {
   document.getElementById(`js-from-${num}`).innerHTML = `${payload[convert].code}`;
   document.getElementById(`js-to-${num}`).innerHTML = `${payload[convert].codein}`;
-  document.getElementById(`js-res-${num}`).innerHTML = `R$${parseFloat(payload[convert].bid).toFixed(2)}`;
+  document.getElementById(`js-res-${num}`).innerHTML = `R$${formatarMoeda(parseFloat(payload[convert].bid))}`;
 }
 
 function fillJs(payload) {
@@ -43,16 +46,13 @@ let ftInput2 = document.getElementById('js-ft-input-2');
 let ftInput3 = document.getElementById('js-ft-input-3');
 
 ftInput1.addEventListener('keyup', () => {
-  // console.log(`1, {${ftInput1.value}} - ${convert1}`)
-  freeConvert('1', ftInput1.value, convert1);
+  freeConvert('1', parseFloat(ftInput1.value), convert1);
 });
 
 ftInput2.addEventListener('keyup', () => {
-  // console.log(`2, {${ftInput2.value}} ${convert2}`)
-  freeConvert('2', ftInput2.value, convert2);
+  freeConvert('2', parseFloat(ftInput2.value), convert2);
 });
 
 ftInput3.addEventListener('keyup', () => {
-  // console.log(`3, {${ftInput3.value}} - ${convert3}`)
-  freeConvert('3', ftInput3.value, convert3);
+  freeConvert('3', parseFloat(ftInput3.value), convert3);
 });
